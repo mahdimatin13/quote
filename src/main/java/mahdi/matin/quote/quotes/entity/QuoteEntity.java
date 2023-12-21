feed5d163411731a23bd111ee32683eb5b201502
mahdimatin13 <mahdi.matin252@gmail.com>
2023-12-22 01:04:59 +1100

    first commit

---
+++ src/main/java/mahdi/matin/quote/quotes/entity/QuoteEntity.java
@@ -0,0 +1,9 @@
+package mahdi.matin.quote.quotes.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "quote_entity")
+public class QuoteEntity {
+}
---
+++ src/test/java/mahdi/matin/quote/QuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package mahdi.matin.quote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
